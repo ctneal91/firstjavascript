c17624697928813a48051965b55f619d999d0248
Christian Neal <c.neal91@gmail.com>
2015-09-29 14:27:03 -0500

    First and Last Commit

---
+++ first.js
@@ -0,0 +1,23 @@
+//#1
+var message = "Hello World";
+console.log(message);
+
+// #2
+var answer = "The answer to life, the universe, and everything is..."
+console.log(answer)
+
+// #3
+var theAnswer = 42
+
+//#4
+console.log(theAnswer.toString())
+
+//#5
+var favoriteBooks = []
+
+favoriteBooks.push("The Outsiders")
+favoriteBooks.push("The Wheel of Time Series")
+favoriteBooks.push("The Color Purple")
+favoriteBooks.push("David Copperfield")
+
+console.log(favoriteBooks)
